b34dc43bf0766609599acc770f53d8fd0384749c
Sonteen <Nattpong16221@gmail.com>
2024-06-13 10:34:42 +0700

    edit code CurrentLogin

--- Day7/front-end/src/components/CurrentLogin.vue
+++ Day7/front-end/src/components/CurrentLogin.vue
@@ -6,7 +6,19 @@ const loginStore = useLoginStore()
 
 <template>
   <div v-if="loginStore.isLogin()">
+    <img 
+      :src="'http://localhost:3000/uploads/' + loginStore.currentUser.image" 
+      alt="Profile image" 
+      class="user_img">
     {{ loginStore.currentUser.login }} is login.
     <button @click="loginStore.logout">Logout</button>
   </div>
 </template>
+
+<style scoped>
+  .user_img{
+    width: 50px;
+    border-radius: 50%;
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
+  }
+</style>
